63becab9f39d2548f230ac7b54fc4676ca9bd5d5
UENDEL SILVEIRA <91839705+UENDEL-SILVEIRA@users.noreply.github.com>
2021-10-30 16:52:52 -0300

    V1.1
    Dados Gráfico atualizado

--- js/grafico.js
+++ js/grafico.js
@@ -1,36 +1,37 @@
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawMultSeries);
+google.charts.load('current', { packages: ['corechart', 'bar'] });
+google.charts.setOnLoadCallback(drawBasic);
 
-function drawMultSeries() {
-    var data = google.visualization.arrayToDataTable([
-        ['Element', 'Horas cursadas', { role: 'style' }, { role: 'annotation' } ],
-        ['HTML5 CSS', 80, '#3366CC', 'Curso em Vídeo'],
-        ['HTML5', 40, '#3366CC', 'Curso em Vídeo'],
-        ['PHP Básico', 40, '#3366CC', 'Curso em Vídeo' ],
-        ['GIT e GITHUB', 20, '#3366CC', 'Curso em Vídeo' ],
-        ['Programador WEB', 200, '#3366CC', 'IFSUL' ],      
-        ['Java Script', 40, '#3366CC', 'Curso em Vídeo' ],
-        ['PHP', 250, '#3366CC', 'Portal Hugo Cursos' ],
-        ['MySql', 40, '#3366CC', 'Portal Hugo Cursos' ],
-        ['MySql', 40, '#3366CC', 'Portal Hugo Cursos' ],
-        ['MySql', 40, '#3366CC', 'Portal Hugo Cursos' ],
-        ['MySql', 40, '#3366CC', 'Portal Hugo Cursos' ],
-        ['MySql', 40, '#3366CC', 'Portal Hugo Cursos' ],
-     ]);
+function drawBasic() {
 
-      var options = {
-        title: '',
-        
-        
-        hAxis: {
-          title: '',
-          minValue: 0
-        },
-        vAxis: {
-          title: ''
-        }
-      };
+  var data = google.visualization.arrayToDataTable([
+    ['', 'Horas Cursadas', { role: 'style' }, { role: 'annotation' }],
+    ['HTML5 CSS', 80, '#3366CC', 'Curso em Vídeo'],
+    ['HTML5', 40, '#3366CC', 'Curso em Vídeo'],
+    ['PHP Básico', 40, '#3366CC', 'Curso em Vídeo'],
+    ['GIT e GITHUB', 20, '#3366CC', 'Curso em Vídeo'],
+    ['Programador WEB', 200, '#3366CC', 'IFSUL'],
+    ['Java Script', 40, '#3366CC', 'Curso em Vídeo'],
+    ['PHP', 160, '#3366CC', 'Portal Hugo Cursos'],
+    ['MySql', 40, '#3366CC', 'Portal Hugo Cursos'],
+    ['AJAX PHP', 20, '#3366CC', 'Q Cursos'],
+    ['ProgramAção', 40, '#3366CC', 'Recode'],
+    ['PHP', 40, '#3366CC', 'IFSUL'],
+    ['Template Ecommerce', 25, '#3366CC', 'Portal Hugo Cursos'],
+  ]);
 
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
+  var options = {
+    title: '',
+    chartArea: { width: '60%' },
+    hAxis: {
+      title: '',
+      minValue: 0
+    },
+    vAxis: {
+      title: ''
     }
+  };
+
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+  chart.draw(data, options);
+}
